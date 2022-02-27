cdc0b07980bb3ec49cd8a04c5abe1ffcc5043598
scottjohnstone <66835704+scottjohnstone@users.noreply.github.com>
2022-02-26 19:05:19 -0500

    Add project files.

---
+++ Calc/Features/Calculator.feature
@@ -0,0 +1,27 @@
+﻿Feature: Calculator
+![Calculator](https://specflow.org/wp-content/uploads/2020/09/calculator.png)
+Simple calculator for adding **two** numbers
+
+Link to a feature: [Calculator](Calc/Features/Calculator.feature)
+***Further read***: **[Learn more about how to generate Living Documentation](https://docs.specflow.org/projects/specflow-livingdoc/en/latest/LivingDocGenerator/Generating-Documentation.html)**
+
+@mytag
+
+Scenario Outline: Add Two Numbers
+	Given I navigated to Standard Calculator
+	And I have entered <SummandOne> into calculator
+	And I press plus
+	And I have entered <SummandTwo> into calculator
+	When I press equal
+	Then the result should be <Result> on the screen
+
+Scenarios: 
+		| SummandOne | SummandTwo | Result |       
+		| 50         | 70         | 120    | 
+		| 1          | 10         | 11     |
+
+Scenario: Calculate two numbers
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
---
+++ Calc/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,127 @@
+﻿using TechTalk.SpecFlow;
+using Xunit;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using System;
+using System.Diagnostics;
+using System.IO;
+using SpecFlowCalculator;
+using FluentAssertions;
+using WindowsAppDriver.Driver;
+
+
+
+namespace Calc.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly Calculator _calculator;
+        private static Process _driver;
+
+
+        public CalculatorStepDefinitions(Calculator calculator)
+        {
+            _calculator = calculator;
+
+        }
+
+        [BeforeTestRun]
+        public static void StartWinAppDriver()
+        {
+            try
+            {
+                var configurationDriver = new ConfigurationDriver();
+                string winAppDriverPath = configurationDriver.Configuration["winAppPath"];
+                _driver = Process.Start(winAppDriverPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not locate WinAppDriver.exe, get it from https://github.com/Microsoft/WinAppDriver/releases and change the winAppPath in app.settings accordingly");
+                throw new FileNotFoundException("Could not locate File WinAppDriver.exe", e);
+            }
+        }
+
+        [AfterTestRun]
+        public static void KillWinAppDriver()
+        {
+            _driver.Kill();
+        }
+
+        [Given(@"I navigated to Standard Calculator")]
+        public void GivenINavigatedToStandard()
+        {
+            _calculator.NavigateToStandard();
+        }
+
+        [Given(@"I have entered (.*) into calculator")]
+        public void GivenIHaveEnteredIntoCalculator(string p0)
+        {
+            _calculator.EnterNumber(p0);
+        }
+
+        [Given(@"I press (.*)")]
+        public void GivenIPress(string p0)
+        {
+            _calculator.EnterSign(p0);
+        }
+
+        [When(@"I press (.*)")]
+        public void WhenIPressEquals(string p0)
+        {
+            _calculator.EnterSign(p0);
+        }
+
+        [Then(@"Calculator title is (.*)")]
+        public void ThenBrowserTitleIs(string p0)
+        {
+            _calculator.GetTitle().Should().Be(p0);
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(string p0)
+        {
+            _calculator.GetResult().Should().Be(p0);
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            _calculator.FirstNumber = number;
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            _calculator.SecondNumber = number;
+        }
+
+        private int _result;
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            _result = _calculator.Add();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            _result.Should().Be(result);
+        }
+    }
+}
+
